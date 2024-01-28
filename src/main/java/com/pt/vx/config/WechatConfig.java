67db85a3a71148802fbc676b83a0f49b4401e157
无力韬韬 <49892512+wul1TT@users.noreply.github.com>
2024-01-28 21:20:01 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -38,13 +38,13 @@ public class WechatConfig {
      */
     static {
         userList.add(getUser(
-                "这个人扫码后的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
-                "南京", //这个人在的城市
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
-                new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
+                "oDYTw6r3grbfbWzIyBqSi9BOBnm8", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "test", //要给这个人发送的模板ID
+                "TT", //咋称呼这个人
+                "山东省济宁市", //这个人的详细地址
+                "济宁", //这个人在的城市
+                new BirthDay(2000,2,25,true,false,"生日快乐！！"),
+                new BirthDay(1999,3,21,false,false,"生日快乐哦~~"),
                 new BirthDay(2020,7,8,true,true),
                 new BirthDay(2020,7,8,true,false,"周年快乐！！！")
         ));
