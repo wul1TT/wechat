cba5a87518130af6845f07e349d17266f5cfe345
无力韬韬 <49892512+wul1TT@users.noreply.github.com>
2024-01-28 23:31:13 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -52,7 +52,7 @@ public class WechatConfig {
         userList.add(getUser(
                 "oDYTw6r3grbfbWzIyBqSi9BOBnm8",
                 "2C9doi_oZCmhVh6LibTaGYvVTtv1TSx2mk1w1xkb7lE",
-                "胖胖",
+                "小胖胖",
                 "山东省烟台市莱山区",
                 "烟台",
                 new BirthDay(1999,3,21,false,false,"生日快乐哦~~"),
